5e95fda927e398388b714da2fc5c40eaf1f099c2
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-04-26 20:23:31 +0300

    Update test scripts & libraries

--- run_test_instance.sh
+++ run_test_instance.sh
@@ -1,19 +1,44 @@
-TASK_SRC_DIR="$1"
-BIN_ROOT="$2"
-JAVAC_COMPILE_FLAGS="$3"
-RUN_TEST_COMMAND="$4"
+TASK_SRC_DIR="$1";
+BIN_ROOT="$2";
+JAVAC_COMPILE_FLAGS="$3";
+RUN_TEST_COMMAND="$4";
+TEST_NAME="$5";
+PROJECT_ROOT_DIR="$PWD";
 
-echo "New target: ${TASK_SRC_DIR}"
+echo;
+echo "######################################";
+echo "Exec test: $TEST_NAME";
 
-echo "Compiling, javac flags: ${JAVAC_COMPILE_FLAGS}"
-if [[ -z $JAVAC_COMPILE_FLAGS ]]; then
-  javac -d "$BIN_ROOT" "$(find "${TASK_SRC_DIR}/"  -name "*.java")";
+if [[ -z $TASK_SRC_DIR ]]; then
+    echo "Task sources path is empty";
+    exit 1;
 else
-  javac -d "$BIN_ROOT" "$JAVAC_COMPILE_FLAGS" "$(find "${TASK_SRC_DIR}/"  -name "*.java")";
+    echo "Task sources path: $TASK_SRC_DIR";
 fi;
 
-echo "Starting tests, command: ${RUN_TEST_COMMAND}"
-cd "$BIN_ROOT" || (echo "Bin root not found" && exit 1);
-eval "$RUN_TEST_COMMAND"
+if [[ -z $BIN_ROOT ]]; then
+    echo "Task binaries out path is empty";
+    exit 1;
+else
+    echo "Task binaries out path: $BIN_ROOT";
+fi;
 
-git clean -f -d;
+
+echo "-------------------------";
+echo "Compiling, javac flags: $JAVAC_COMPILE_FLAGS";
+if [[ -z $JAVAC_COMPILE_FLAGS ]]; then
+  javac -d "$BIN_ROOT" "$(find "$TASK_SRC_DIR/"  -name "*.java")";
+else
+  javac -d "$BIN_ROOT" "$JAVAC_COMPILE_FLAGS" "$(find "$TASK_SRC_DIR/"  -name "*.java")";
+fi;
+
+echo "-------------------------";
+echo "Starting tests, command: $RUN_TEST_COMMAND";
+cd "$BIN_ROOT" || (echo "['cd' ERR] Bin root not found" && exit 1);
+eval "$RUN_TEST_COMMAND";
+
+echo "-------------------------";
+CLEAN_COMMAND="git clean -f -d";
+echo "Cleaning ($CLEAN_COMMAND)...";
+cd "$PROJECT_ROOT_DIR" || (echo "['cd' ERR] Couldn't return to project root" && exit 1);
+eval "$CLEAN_COMMAND";
--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 
 TESTS_INFO_PATH="./test_info.txt";
 
-awk -F ':' '{
-    system("bash ./run_test_instance.sh " "\"" $1 "\" \"" $2 "\" \"" $3 "\" \"" $4 "\"");
-  }' "$TESTS_INFO_PATH"
+# SOURCES_DIR:BUILD_OUT_DIR:JAVAC_FLAGS:RUN_TESTING_COMMAND:TEST_NAME
+awk -F ':' 'NF == 5 {
+    system("bash ./run_test_instance.sh " "\"" $1 "\" \"" $2 "\" \"" $3 "\" \"" $4 "\" \"" $5 "\"");
+  }' "$TESTS_INFO_PATH";
